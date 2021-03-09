558465e49e8563f08727e76140de46d2ec579860
conache <cconache@gmail.com>
2021-03-09 21:50:06 +0200

    Fixed favicon and facebook social url

--- src/components/team/Team.jsx
+++ src/components/team/Team.jsx
@@ -6,7 +6,6 @@ import {ReactComponent as YoutubeLogo} from "../../assets/yt_icon.svg";
 
 class Team extends React.Component {
   render() {
-    const url = "https://www.facebook.com/trupadeteatru.protha";
     return <div className="custom-section team-section">
         <div className="container d-flex align-content-center justify-content-center flex-wrap">
             <div className="organiser-text">Festival organizat de</div>
@@ -14,7 +13,7 @@ class Team extends React.Component {
                 <img className="col-lg-3 col-md-4 col-sm-6 col-6 trupa-logo" src={LogoTrupa} />
             </div>
             <div className="social-logos-container">
-                <a href="https://www.facebook.com/trupadeteatru.protha" target="_blank" ><FacebookLogo className="social-logo" /></a>
+                <a href="https://www.facebook.com/oradeteatru" target="_blank" ><FacebookLogo className="social-logo" /></a>
                 <a href="https://www.youtube.com/channel/UCr_GbCvgXSb9sH3gjv8qvJQ"><YoutubeLogo className="social-logo" /></a>
                 <a href="https://www.instagram.com/trupa.protha/"><InstagramLogo className="social-logo" /></a>
             </div>
