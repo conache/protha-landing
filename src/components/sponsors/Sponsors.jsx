1ad33b1272dd4abd1098c55c2879e8b8cadb91e2
conache <cconache@gmail.com>
2021-03-10 11:19:56 +0200

    More changes based on feedback

--- src/components/sponsors/Sponsors.jsx
+++ src/components/sponsors/Sponsors.jsx
@@ -2,14 +2,22 @@ import React from 'react';
 import LogoNuclear from "../../assets/logo_nuclear.jpeg";
 import LogoVexio from "../../assets/logo_vexio.png";
 import LogoMercury from "../../assets/logo_mercury.jpg";
+import LogoTrupa from "../../assets/logo_trupa.png";
 import LogoConsiliulJudeteanVrancea from "../../assets/logo_cjv.png";
 
 
 class Sponsors extends React.Component {
   render() {
     return [
+      <div className="container custom-section">
+      <div className="responsive-title custom-section-title">Festival organizat de</div>
+      <div className="row justify-content-center d-flex flex-column align-items-center">
+          <img className="col-lg-3 col-md-4 col-sm-7 col-xs-6 clickable-logo asociatie-logo" src={LogoTrupa} onClick={() => window.open("https://www.facebook.com/trupadeteatru.protha", "_blank")} />
+          <div className="organiser-text clickable-logo" onClick={() => window.open("https://www.facebook.com/trupadeteatru.protha", "_blank")}>Asociatia Protha Panciu</div>
+        </div>
+    </div>,
     <div className="container custom-section">
-      <div className="responsive-title custom-section-title">Festival realizat cu sprijinul</div>
+      <div className="responsive-title custom-section-title">Realizat cu sprijinul</div>
       <div className="row justify-content-center">
         <img className="col-md-5 col-sm-10 col-xs-6 clickable-logo" src={LogoConsiliulJudeteanVrancea} onClick={() => window.open("https://cjvrancea.ro/", "_blank")} />
       </div>
--- src/components/team/Team.jsx
+++ src/components/team/Team.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import LogoTrupa from "../../assets/logo_trupa.png";
+import LogoFestival from "../../assets/bottom-logo.png";
 import {ReactComponent as FacebookLogo} from  "../../assets/fb_icon.svg";
 import {ReactComponent as InstagramLogo} from "../../assets/ig_icon.svg";
 import {ReactComponent as YoutubeLogo} from "../../assets/yt_icon.svg";
@@ -8,10 +8,7 @@ class Team extends React.Component {
   render() {
     return <div className="custom-section team-section">
         <div className="container d-flex align-content-center justify-content-center flex-wrap">
-            <div className="organiser-text">Festival organizat de</div>
-            <div className="row justify-content-center">
-                <img className="col-lg-3 col-md-4 col-sm-6 col-6 trupa-logo" src={LogoTrupa} />
-            </div>
+            <img className="col-lg-4 col-md-6 col-sm-8 col-8 festival-logo" src={LogoFestival} />
             <div className="social-logos-container">
                 <a href="https://www.facebook.com/oradeteatru" target="_blank" ><FacebookLogo className="social-logo" /></a>
                 <a href="https://www.youtube.com/channel/UCr_GbCvgXSb9sH3gjv8qvJQ"><YoutubeLogo className="social-logo" /></a>
