d8c3809694f81b7917f1fbb23e22a6f05bea1e11
Cristian Conache <C.conache@gmail.com>
2023-06-15 23:51:46 -0700

    2023 short revamp (#39)
    * 2023 revamp
    
    * Try trigger deployment
    
    * Remove yarn lock

--- src/components/sponsors/Sponsors.jsx
+++ src/components/sponsors/Sponsors.jsx
@@ -1,139 +1,217 @@
 import React from 'react';
-import LogoNuclear from "../../assets/logo_nuclear.jpeg";
-import LogoVexio from "../../assets/logo_vexio.png";
-import LogoMercury from "../../assets/logo_mercury.jpg";
-import LogoTrupa from "../../assets/logo_trupa.png";
-import LogoConsiliulJudeteanVrancea from "../../assets/NewLogoCJV.jpg";
-import LogoZagaZaga from "../../assets/logo-ZagaZagaSat.png";
-import LogoGrupAutonom from "../../assets/LogoGrupAutonom.png";
-import LogoBTD from "../../assets/btdLogo.png";
-import LogoLaBunelu from "../../assets/LogoLaBunelu.jpg"
-import LogoPaco from "../../assets/LogoPaco.png";
-import LogoRobutex from "../../assets/LogoRobutex.jpeg";
-import LogoSculePanciu from "../../assets/LogoSculePanciu.png";
-import LogoBeciulDomnesc from "../../assets/LogoBeciulDomnesc.jpeg";
-import LogoMarco from "../../assets/LogoMarco.jpeg";
-import LogoDavide from "../../assets/LogoDavide.jpeg";
-import LogoFlorariaMaruca from "../../assets/LogoFlorariaMaruca.png";
-import LogoLupinocom from "../../assets/LogoLupinocom.png";
-import LogoPlexity from "../../assets/LogoPlexity.png";
-import LogoCramaValentePanciu from "../../assets/LogoCramaValentePanciu.png";
-import LogoGradinaCuCamelii from "../../assets/GradinaCuCamelii.jpeg";
-import LogoCosmeticBYBCars from "../../assets/LogoCosmeticBYBCars.png";
-import LogoTricottonJunior from "../../assets/LogoTricottonJunior.png";
-
-
-
-
-{/* Separat sectiunea Sponsori in Sponsor principal si Sponsor,
-    Sters sponsori, cu exceptia Vexio, si adaugat sub forma de text "Altex Prod." */}
+import LogoVexio from '../../assets/logo_vexio.png';
+import LogoTrupa from '../../assets/logo_trupa.png';
+import LogoConsiliulJudeteanVrancea from '../../assets/NewLogoCJV.jpg';
+import LogoZagaZaga from '../../assets/logo-ZagaZagaSat.png';
+import LogoGrupAutonom from '../../assets/LogoGrupAutonom.png';
+import LogoBTD from '../../assets/btdLogo.png';
+import LogoLaBunelu from '../../assets/LogoLaBunelu.jpg';
+import LogoPaco from '../../assets/LogoPaco.png';
+import LogoRobutex from '../../assets/LogoRobutex.jpeg';
+import LogoSculePanciu from '../../assets/LogoSculePanciu.png';
+import LogoBeciulDomnesc from '../../assets/LogoBeciulDomnesc.jpeg';
+import LogoMarco from '../../assets/LogoMarco.jpeg';
+import LogoDavide from '../../assets/LogoDavide.jpeg';
+import LogoFlorariaMaruca from '../../assets/LogoFlorariaMaruca.png';
+import LogoLupinocom from '../../assets/LogoLupinocom.png';
+import LogoPlexity from '../../assets/LogoPlexity.png';
+import LogoCramaValentePanciu from '../../assets/LogoCramaValentePanciu.png';
+import LogoGradinaCuCamelii from '../../assets/GradinaCuCamelii.jpeg';
+import LogoCosmeticBYBCars from '../../assets/LogoCosmeticBYBCars.png';
+import LogoTricottonJunior from '../../assets/LogoTricottonJunior.png';
 
 class Sponsors extends React.Component {
   render() {
     return [
       <div className="container custom-section">
-      <div className="responsive-title custom-section-title">Host</div>
-      <div className="row justify-content-center d-flex flex-column align-items-center">
-          <img className="col-lg-3 col-md-4 col-sm-7 col-xs-6 clickable-logo asociatie-logo" src={LogoTrupa} onClick={() => window.open("https://www.facebook.com/trupadeteatru.protha", "_blank")} />
-          <div className="organiser-text clickable-logo" onClick={() => window.open("https://www.facebook.com/trupadeteatru.protha", "_blank")}>Asociatia Protha Panciu</div>
+        <div className="responsive-title custom-section-title">Host</div>
+        <div className="row justify-content-center d-flex flex-column align-items-center">
+          <img
+            className="col-lg-3 col-md-4 col-sm-7 col-xs-6 clickable-logo asociatie-logo"
+            src={LogoTrupa}
+            onClick={() => window.open('https://www.facebook.com/trupadeteatru.protha', '_blank')}
+          />
+          <div
+            className="organiser-text clickable-logo"
+            onClick={() => window.open('https://www.facebook.com/trupadeteatru.protha', '_blank')}
+          >
+            Asociatia Protha Panciu
+          </div>
         </div>
-    </div>,
-    <div className="container custom-section">
-      <div className="responsive-title custom-section-title">Co-Host</div>
+      </div>,
+      <div className="container custom-section">
+        <div className="responsive-title custom-section-title">Co-Host</div>
         <div className="row justify-content-center">
           {/* 
           <img className="col-md-6 col-sm-10 col-xs-6 clickable-logo" src={LogoNuclear} onClick={() => window.open("https://www.nuclearelectrica.ro/", "_blank")} />
           <img className="col-md-5 col-sm-12 clickable-logo" src={LogoVexio} onClick={() => window.open("https://www.vexio.ro/", "_blank")} />
           <img className="col-md-5 col-sm-12 clickable-logo" src={LogoZagaZaga} onClick={() => window.open("https://zagazaga.ro/", "_blank")} />
           */}
-          <img className="col-md-5 col-sm-12 clickable-logo" style={{ marginBottom: 30, maxWidth: '80%', height:'80%',  }} 
-              src={LogoZagaZaga} onClick={() => window.open("https://zagazaga.ro/ora-de-teatru-2022/", "_blank")} />
+          <img
+            className="col-md-5 col-sm-12 clickable-logo"
+            style={{ marginBottom: 30, maxWidth: '80%', height: '80%' }}
+            src={LogoZagaZaga}
+            onClick={() => window.open('https://zagazaga.ro/ora-de-teatru-2022/', '_blank')}
+          />
         </div>
         <div className="row d-flex justify-content-between align-items-center sponsors-secondary-row">
           {/*<img className="col-md-5 col-sm-12 clickable-logo" src={LogoMercury} onClick={() => window.open("https://mercury360.ro/", "_blank")} />*/}
         </div>
-    <div >
-    <div className="container custom-section">
-      <div className="responsive-title custom-section-title">Realizat cu sprijinul</div>
-      <div className="row justify-content-center">
-        <img className="col-md-5 col-sm-10 col-xs-6 clickable-logo" src={LogoConsiliulJudeteanVrancea} onClick={() => window.open("https://cjvrancea.ro/", "_blank")} />
-      </div>
-    </div>,
-    <div className="responsive-title custom-section-title">Sponsori</div>
-      <div className="row justify-content-center">
-      <img className="col-md-5 col-sm-12 clickable-logo" src={LogoBTD} onClick={() => window.open("https://www.btdconstruct.ro/", "_blank")} 
-            style={{ marginBottom: 75}}/>
-      </div>
-      <div className="row justify-content-center">
-      <img className="col-md-5 col-sm-12 clickable-logo" src={LogoBeciulDomnesc} onClick={() => window.open("https://beciuldomnesc.ro/?gclid=CjwKCAjw3qGYBhBSEiwAcnTRLl5tGepAjm3VTams6fHsetx18uxjXg4CoHSYF94qYpZlam9zXpVVSRoCG_8QAvD_BwE", "_blank")} 
-            style={{ marginBottom: 75}}/>
-      </div>
-      <div className="row justify-content-center">
-      <img className="col-md-5 col-sm-12" src={LogoTricottonJunior}
-            style={{ marginBottom: 75, maxWidth: '65%', height:'auto'}}/>
-      </div>
-      <div className="row justify-content-center">
-      <img className="col-md-5 col-sm-12 clickable-logo" src={LogoPaco} onClick={() => window.open("https://pacomarket.ro/", "_blank")} 
-            style={{ marginBottom: 75}}/>
-      </div>
-      <div className="row justify-content-center">
-      <img className="col-md-5 col-sm-12 clickable-logo" src={LogoVexio} onClick={() => window.open("https://www.vexio.ro/", "_blank")} 
-            style={{ marginBottom: 75}}/>
-      </div>
-      <div >
-      <div className="row justify-content-center">
-      <img className="col-md-5 col-sm-12 clickable-logo" src={LogoGrupAutonom} onClick={() => window.open("https://www.autonom.com/about", "_blank")} 
-            style={{ marginBottom: 75}}/>
-      </div>
-      <div className="row justify-content-center">
-      <img className="col-md-5 col-sm-12 clickable-logo" src={LogoSculePanciu} onClick={() => window.open("http://www.sculepanciu.com/", "_blank")} 
-            style={{ marginBottom: 75}}/>
-      </div>
-      <div className="row justify-content-center">
-      <img className="col-md-5 col-sm-12" src={LogoLupinocom}  
-            style={{ marginBottom: 75}}/>
-      </div>
-      <div className="row justify-content-center">
-      <img className="col-md-5 col-sm-12 clickable-logo" src={LogoRobutex} onClick={() => window.open("http://www.robutex.ro/", "_blank")} 
-            style={{ marginBottom: 75}}/>
-      </div>
-      <div className="row justify-content-center">
-      <img className="col-md-5 col-sm-12 clickable-logo" src={LogoMarco} onClick={() => window.open("https://www.facebook.com/BrutariaMarco", "_blank")} 
-            style={{ marginBottom: 75}}/>
-      </div>
-      <div className="row justify-content-center">
-      <img className="col-md-5 col-sm-12 clickable-logo" src={LogoLaBunelu} onClick={() => window.open("https://www.facebook.com/profile.php?id=100067471712726", "_blank")} 
-            style={{ marginBottom: 75, maxWidth: '65%', height:'auto'}}/>
-      </div>
-      <div className="row justify-content-center">
-      <img className="col-md-5 col-sm-12 clickable-logo" src={LogoCramaValentePanciu} onClick={() => window.open("https://www.facebook.com/profile.php?id=100067187064281", "_blank")} 
-            style={{ marginBottom: 75, maxWidth: '65%', height:'auto'}}/>
-      </div>
-      <div className="row justify-content-center">
-      <img className="col-md-5 col-sm-12" src={LogoPlexity}  
-            style={{ marginBottom: 75}}/>
-      </div>
-      <div className="row justify-content-center">
-      <img className="col-md-5 col-sm-12 clickable-logo" src={LogoFlorariaMaruca} onClick={() => window.open("https://www.facebook.com/florariamarucapanciu", "_blank")} 
-            style={{ marginBottom: 75, maxWidth: '65%', height:'auto'}}/>
-      </div>
-      <div className="row justify-content-center">
-      <img className="col-md-5 col-sm-12 clickable-logo" src={LogoGradinaCuCamelii} 
-            style={{ marginBottom: 75, maxWidth: '65%', height:'auto'}}/>
-      </div>
-      <div className="row justify-content-center">
-      <img className="col-md-5 col-sm-12 clickable-logo" src={LogoDavide} onClick={() => window.open("https://www.facebook.com/restaurantladavide", "_blank")} 
-            style={{ marginBottom: 75, maxWidth: '65%', height:'auto'}}/>
-      </div>
-      <div className="row justify-content-center">
-      <img className="col-md-5 col-sm-12" src={LogoCosmeticBYBCars} 
-            style={{ marginBottom: 75, maxWidth: '65%', height:'auto'}}/>
-      </div>
-      </div>
-    </div>
-  </div>
-    
-    ]
+        <div>
+          <div className="container custom-section">
+            <div className="responsive-title custom-section-title">Realizat cu sprijinul</div>
+            <div className="row justify-content-center">
+              <img
+                className="col-md-5 col-sm-10 col-xs-6 clickable-logo"
+                src={LogoConsiliulJudeteanVrancea}
+                onClick={() => window.open('https://cjvrancea.ro/', '_blank')}
+              />
+            </div>
+          </div>
+          ,<div className="responsive-title custom-section-title">Sponsori</div>
+          <div className="row justify-content-center">
+            <img
+              className="col-md-5 col-sm-12 clickable-logo"
+              src={LogoBTD}
+              onClick={() => window.open('https://www.btdconstruct.ro/', '_blank')}
+              style={{ marginBottom: 75 }}
+            />
+          </div>
+          <div className="row justify-content-center">
+            <img
+              className="col-md-5 col-sm-12 clickable-logo"
+              src={LogoBeciulDomnesc}
+              onClick={() =>
+                window.open(
+                  'https://beciuldomnesc.ro/?gclid=CjwKCAjw3qGYBhBSEiwAcnTRLl5tGepAjm3VTams6fHsetx18uxjXg4CoHSYF94qYpZlam9zXpVVSRoCG_8QAvD_BwE',
+                  '_blank',
+                )
+              }
+              style={{ marginBottom: 75 }}
+            />
+          </div>
+          <div className="row justify-content-center">
+            <img
+              className="col-md-5 col-sm-12"
+              src={LogoTricottonJunior}
+              style={{ marginBottom: 75, maxWidth: '65%', height: 'auto' }}
+            />
+          </div>
+          <div className="row justify-content-center">
+            <img
+              className="col-md-5 col-sm-12 clickable-logo"
+              src={LogoPaco}
+              onClick={() => window.open('https://pacomarket.ro/', '_blank')}
+              style={{ marginBottom: 75 }}
+            />
+          </div>
+          <div className="row justify-content-center">
+            <img
+              className="col-md-5 col-sm-12 clickable-logo"
+              src={LogoVexio}
+              onClick={() => window.open('https://www.vexio.ro/', '_blank')}
+              style={{ marginBottom: 75 }}
+            />
+          </div>
+          <div>
+            <div className="row justify-content-center">
+              <img
+                className="col-md-5 col-sm-12 clickable-logo"
+                src={LogoGrupAutonom}
+                onClick={() => window.open('https://www.autonom.com/about', '_blank')}
+                style={{ marginBottom: 75 }}
+              />
+            </div>
+            <div className="row justify-content-center">
+              <img
+                className="col-md-5 col-sm-12 clickable-logo"
+                src={LogoSculePanciu}
+                onClick={() => window.open('http://www.sculepanciu.com/', '_blank')}
+                style={{ marginBottom: 75 }}
+              />
+            </div>
+            <div className="row justify-content-center">
+              <img
+                className="col-md-5 col-sm-12"
+                src={LogoLupinocom}
+                style={{ marginBottom: 75 }}
+              />
+            </div>
+            <div className="row justify-content-center">
+              <img
+                className="col-md-5 col-sm-12 clickable-logo"
+                src={LogoRobutex}
+                onClick={() => window.open('http://www.robutex.ro/', '_blank')}
+                style={{ marginBottom: 75 }}
+              />
+            </div>
+            <div className="row justify-content-center">
+              <img
+                className="col-md-5 col-sm-12 clickable-logo"
+                src={LogoMarco}
+                onClick={() => window.open('https://www.facebook.com/BrutariaMarco', '_blank')}
+                style={{ marginBottom: 75 }}
+              />
+            </div>
+            <div className="row justify-content-center">
+              <img
+                className="col-md-5 col-sm-12 clickable-logo"
+                src={LogoLaBunelu}
+                onClick={() =>
+                  window.open('https://www.facebook.com/profile.php?id=100067471712726', '_blank')
+                }
+                style={{ marginBottom: 75, maxWidth: '65%', height: 'auto' }}
+              />
+            </div>
+            <div className="row justify-content-center">
+              <img
+                className="col-md-5 col-sm-12 clickable-logo"
+                src={LogoCramaValentePanciu}
+                onClick={() =>
+                  window.open('https://www.facebook.com/profile.php?id=100067187064281', '_blank')
+                }
+                style={{ marginBottom: 75, maxWidth: '65%', height: 'auto' }}
+              />
+            </div>
+            <div className="row justify-content-center">
+              <img className="col-md-5 col-sm-12" src={LogoPlexity} style={{ marginBottom: 75 }} />
+            </div>
+            <div className="row justify-content-center">
+              <img
+                className="col-md-5 col-sm-12 clickable-logo"
+                src={LogoFlorariaMaruca}
+                onClick={() =>
+                  window.open('https://www.facebook.com/florariamarucapanciu', '_blank')
+                }
+                style={{ marginBottom: 75, maxWidth: '65%', height: 'auto' }}
+              />
+            </div>
+            <div className="row justify-content-center">
+              <img
+                className="col-md-5 col-sm-12 clickable-logo"
+                src={LogoGradinaCuCamelii}
+                style={{ marginBottom: 75, maxWidth: '65%', height: 'auto' }}
+              />
+            </div>
+            <div className="row justify-content-center">
+              <img
+                className="col-md-5 col-sm-12 clickable-logo"
+                src={LogoDavide}
+                onClick={() => window.open('https://www.facebook.com/restaurantladavide', '_blank')}
+                style={{ marginBottom: 75, maxWidth: '65%', height: 'auto' }}
+              />
+            </div>
+            <div className="row justify-content-center">
+              <img
+                className="col-md-5 col-sm-12"
+                src={LogoCosmeticBYBCars}
+                style={{ marginBottom: 75, maxWidth: '65%', height: 'auto' }}
+              />
+            </div>
+          </div>
+        </div>
+      </div>,
+    ];
   }
 }
 
